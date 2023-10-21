9cf6ffba31731a21b6ec3aceaa54aefaef62c105
jojoch <vzelinkova@email.cz>
2023-10-21 12:22:22 +0200

    Disable graph

--- Metis/Scene/DashboardFlow/UI/DashboardView.swift
+++ Metis/Scene/DashboardFlow/UI/DashboardView.swift
@@ -87,6 +87,7 @@ private extension DashboardView {
                             dropShadowColor: .white
                         )
                     )
+                    .disabled(true)
                 }
             }
             .padding([.leading, .trailing], 24)
